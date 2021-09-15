36d6fcf35cf3ddec546251869029a63f18c6a7b1
deveb1479@gmail.com <deveb1479@gmail.com>
2021-09-16 04:16:51 +0900

    ...

--- MyDot/Properties/Resources.Designer.cs
+++ MyDot/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Dotpia.Properties {
+namespace Mydot.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Dotpia.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Dotpia.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mydot.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
